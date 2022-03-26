3f06b5483ccb74012b5c39796e9787e1ccc38211
wojciech.laciak <wojciech.laciak@PK1-14>
2022-03-26 19:21:45 +0100

    Dodaj pliki projektów.

---
+++ Lab02/Fraction.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab02
+{
+    class Compare : IComparer<Fraction>
+    {
+        int IComparer<Fraction>.Compare(Fraction fraction1, Fraction fraction2)
+        {
+            int commonDenominator = fraction1.Denominator * fraction2.Denominator;
+            int thisNumerator = commonDenominator / fraction1.Denominator * fraction1.Numerator;
+            int oterNumerator = commonDenominator / fraction2.Denominator * fraction2.Numerator;
+
+            return thisNumerator - oterNumerator;
+        }
+    }
+
+    class Fraction: IComparable<Fraction>
+    {
+        public int Numerator { get; }
+        public int Denominator { get; }
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("denominator shouldn't be equal 0");
+            }
+
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override String ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        public int CompareTo(Fraction otherFraction)
+        {
+            int commonDenominator = this.Denominator * otherFraction.Denominator;
+            int thisNumerator = commonDenominator / this.Denominator * this.Numerator;
+            int oterNumerator = commonDenominator / otherFraction.Denominator * otherFraction.Numerator;
+
+            return thisNumerator - oterNumerator;
+
+        }
+
+        public Fraction Add(Fraction otherFraction)
+        {
+            int commonDenominator = this.Denominator * otherFraction.Denominator;
+            int numerator1 = commonDenominator / this.Denominator * this.Numerator;
+            int numerator2 = commonDenominator / otherFraction.Denominator * otherFraction.Numerator;
+
+            return new Fraction(numerator1 + numerator2, commonDenominator);
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1.Add(fraction2);
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            int commonDenominator = fraction1.Denominator * fraction2.Denominator;
+            int numerator1 = commonDenominator / fraction1.Denominator * fraction1.Numerator;
+            int numerator2 = commonDenominator / fraction2.Denominator * fraction2.Numerator;
+
+            return new Fraction(numerator1 - numerator2, commonDenominator);
+        }
+
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.Numerator * fraction2.Numerator, fraction1.Denominator * fraction2.Denominator);
+        }
+
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1 * fraction2.Inverse();
+        }
+
+        public Fraction Inverse()
+        {
+            return new Fraction(this.Denominator, this.Numerator);
+        }
+
+    }
+}
---
+++ Lab02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new(1, 3);
+            Fraction fraction2 = new(1, 2);
+            Fraction fraction3 = new(1, 6);
+
+            Console.WriteLine(fraction1 + fraction2);
+            Console.WriteLine(fraction1 -  fraction2);
+            Console.WriteLine(fraction1 * fraction2);
+            Console.WriteLine(fraction1 / fraction2);
+
+            List<Fraction> list = new();
+            list.Add(fraction1);
+            list.Add(fraction2);
+            list.Add(fraction3);
+
+            Console.WriteLine("List before sorting");
+            list.Sort();
+            Console.WriteLine("List after sorting");
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
